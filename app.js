f91f682d67d4ae02833a97621f67fda56645aa13
Jackie Barbetta <jackieb@us.ibm.com>
2016-09-12 11:39:44 -0400

    Added webhook/bot frontend

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var express = require('express'),
     user = require('./routes/user'),
     http = require('http'),
     path = require('path'),
+    //querystring = require('querystring'),
+    request = require('request'),
     WebSocketServer = require('ws').Server;
 
 var app = express();
@@ -17,6 +19,8 @@ var wss;
 // all environments
 app.set('port',  process.env.TRACKER_PORT || 3000);
 app.set('host',  process.env.TRACKER_HOST || 'localhost');
+app.set('bothost',  process.env.BOTHOST || null);
+app.set('botservice',  process.env.BOTSERVICE || null);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -63,25 +67,71 @@ wss.on('connection', function(ws) {
     if (msg && msg.color) {
         // do whatever
         console.log(msg.color+" postIt note detected");
+        postToBot("The cat says......you are holding a "+msg.color+" postIt note!  Meow!");
     } else if (msg && msg.expression) {
         console.log(msg.expression+" detected");
     }
   });
 });
 
-/*
-trackerbot.startUpdates(function(data){
-    // we got data from the bot 
-    var data = JSON.parse(data);
-    if (data && data.type === 'emotion') {
-        //console.log("Got emotion data from trackerbot: ",JSON.stringify(data));
-        // If we need to do anything with the data now is our chance.
-        handleEmotion(data);
-    }
-});
-*/
+function postToBot(message) {
+    var webhook_host = app.get('bothost');
+    var webhook_path = app.get('botservice');
 
-function handleEmotion(data) {
+    if (!webhook_host || !webhook_path) {
+        console.log("NO BOTURL CONFIGURED.  CANNOT POST TO BOT");
+        return;
+    }
+
+    var post_data = {
+        channel: "#random",
+        username: "catbot",
+        text: message,
+        icon_emoji: ":cat2:"
+    };
+
+    var post_options = {
+        host: webhook_host,
+        path: webhook_path,
+        method: 'POST',
+        headers: {
+            //'Content-Type': 'application/x-www-form-urlencoded',
+            'Context-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(post_data)
+        }
+    };
+
+    console.log("Sending: ",post_data);
+    console.log("with opts: ",post_options);
+
+/*
+    var post_req = http.request(post_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+
+    post_req.on('error', function(e) {
+        console.log("problem with request: ",e.message);
+    });
+
+    post_req.write(post_data);
+    post_req.end(); 
+*/
+    var url = webhook_host+webhook_path;
+
+    request.post(url, 
+        {json: post_data},
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body)
+            }
+        }
+    );
+}
+
+function handleColor(data) {
     wss.clients.forEach(function(client){
         try {
             //console.log("Sending emotion data to browser: ",JSON.stringify(data));
